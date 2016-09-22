3fb782ace005a16a4c23178c96dcdf5c472dcd00
Simone Lee <simonelee@live.dk>
2016-09-22 08:14:05 +0200

    Comments added for the menu.php and require
    Added comments of how the menu.php works and the “require” function.

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
 </head>
 
 <body>
-	<ul>
 <?php 
+/*Instead of having the individual code for the menu in each page, this php connects to one php file that has the entire menu. So if anything needs to be changed, it only has to be changed in the one menu.php file and the other pages will update.
+The "require" function means that the php will look for the files from the seperate menu.php file and the process will stop, if any files are missing. Instead could've used "include" but if any files should be missing, the php will just ignore this and continue with the other links. As all the files for the menu should be working, I used "require".*/
 require 'menu.php';
 ?>
 <h1>This is my home page.</h1>
