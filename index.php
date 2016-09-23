0d16be55e544418109ccfac8910a575469698828
Simone Lee <simonelee@live.dk>
2016-09-23 08:08:34 +0200

    PHP Menu active class
    I’ve created the variable curpage for the current page on each .php
    file and then added the conditional statement on the menu.php to show
    that when one of the pages are active it should have the class
    “active”. This is for the styling later.

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
 
 <body>
 <?php 
+
+/*To show which page is the active for the menu, I've created the variable $curpage for the current page. */
+$curpage = 'index.php'; 
+
+	
 /*Instead of having the individual code for the menu in each page, this php connects to one php file that has the entire menu. So if anything needs to be changed, it only has to be changed in the one menu.php file and the other pages will update.
 The "require" function means that the php will look for the files from the seperate menu.php file and the process will stop, if any files are missing. Instead could've used "include" but if any files should be missing, the php will just ignore this and continue with the other links. As all the files for the menu should be working, I used "require".*/
 require 'menu.php';
--- menu.php
+++ menu.php
@@ -1,6 +1,16 @@
+<!-- Afterwards I've used a conditional statement that if the file .php (depending on which one is active) is the current/active page it should be in the class "active". -->
+
 <ul>
-  <li><a href="index.php">Home</a></li>
-  <li><a href="about.php">About</a></li>
-  <li><a href="news.php">News</a></li>
-  <li><a href="contact.php">Contact</a></li>
+  <li><a href="index.php" <?php if($curpage == 'index.php') {
+	echo 'class="active"';
+	} ?>>Home</a></li>
+  <li><a href="about.php" <?php if($curpage == 'about.php') {
+	echo 'class="active"';
+	}?>>About</a></li>
+  <li><a href="news.php" <?php if($curpage == 'news.php') {
+	echo 'class="active"';
+	}?>>News</a></li>
+  <li><a href="contact.php" <?php if($curpage == 'contact.php') {
+	echo 'class="active"';
+	} ?>>Contact</a></li>
 </ul>
